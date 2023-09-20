323de5c7e0de4c44b4dd9de7e642deddeb0b8419
Alessio Marchi <65831826+kettei-sproutty@users.noreply.github.com>
2023-09-21 01:20:00 +0200

    chore: project setup

--- packages/parser/src/lib.rs
+++ packages/parser/src/lib.rs
@@ -1,6 +1,8 @@
 extern crate wasm_bindgen;
 
 use wasm_bindgen::prelude::*;
+use wasm_bindgen_test::console_log;
+use web_sys::ReadableStream;
 
 #[wasm_bindgen]
 extern {
@@ -8,6 +10,13 @@ extern {
 }
 
 #[wasm_bindgen]
-pub fn greet() {
-    alert("Hello, World!");
+pub fn greet(text: &str) {
+    alert(text);
+}
+
+#[wasm_bindgen]
+pub async fn log(file: ReadableStream) -> String {
+  let _reader = file.get_reader();
+
+  "Hello, World!".to_owned()
 }
