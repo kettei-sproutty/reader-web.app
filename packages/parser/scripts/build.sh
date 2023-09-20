c390ff18effe06729b1fa63c262a714e55aa909b
Alessio Marchi <65831826+kettei-sproutty@users.noreply.github.com>
2023-09-20 23:40:55 +0200

    chore: project setup

--- packages/parser/scripts/build.sh
+++
@@ -1,9 +0,0 @@
-if [ "$(uname)" == "Darwin" ]; then
-  export CARGO_HOME=$HOME/.cargo
-
-  wasm-pack build --release
-elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
-  export CARGO_HOME=/vercel/path0/.cargo
-
-  $CARGO_HOME/bin/wasm-pack build --release
-fi
--- packages/parser/scripts/test.sh
+++
@@ -1,9 +0,0 @@
-if [ "$(uname)" == "Darwin" ]; then
-  export CARGO_HOME=$HOME/.cargo/
-
-  wasm-pack test --firefox --headless
-elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
-  export CARGO_HOME=/vercel/path0/.cargo
-
-  wasm-pack test --firefox --headless
-fi
