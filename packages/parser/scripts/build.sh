38abe9595fdeaa653e0c7ac926ed16fc8ea3ef18
Alessio Marchi <65831826+kettei-sproutty@users.noreply.github.com>
2023-09-21 00:29:59 +0200

    chore: project setup

--- packages/parser/scripts/build.sh
+++ packages/parser/scripts/build.sh
@@ -1,9 +1,7 @@
 if [ "$(uname)" == "Darwin" ]; then
     wasm-pack build --release
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
-    echo $CARGO_HOME
-    export CARGO_HOME=/vercel/path0/.cargo
-    echo $CARGO_HOME
+    ls $HOME
     source $CARGO_HOME/env
 
     wasm-pack build --release
